a9a8b94e1a2701d2aef0542cf43c5a5b22c4eaa6
Christian Harborow <christianharborow@hotmail.co.uk>
2018-11-12 17:36:26 +0000

    added description and author info to decimal_to_binary

--- Week 6/decimal_to_binary.py
+++ Week 6/decimal_to_binary.py
@@ -1,3 +1,14 @@
+"""
+    Decimal To Binary Converter
+
+    Prints out a column of the decimal integers 0 to 127 alongside another column
+    containing their 8-bit binary equivalents
+"""
+
+__author__ = "Christian Harborow"
+__email__ = "u1856364@unimail.hud.ac.uk"
+__date__ = "12/11/2018"
+
 for decimal in range(0, 128):
     binary = bin(decimal)[2:]
     print(" " * (3-len(str(decimal))) + str(decimal), "0" * (8-len(binary)) + binary)
