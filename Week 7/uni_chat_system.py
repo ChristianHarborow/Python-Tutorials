6f8e2fa6a662ed4230f545adb3dd7f78bdae50a3
Christian Harborow <christianharborow@hotmail.co.uk>
2018-11-19 18:31:15 +0000

    minor changes to uni_chat_system

--- Week 7/uni_chat_system.py
+++ Week 7/uni_chat_system.py
@@ -1,5 +1,15 @@
 import random
 
+"""
+    Student Support Chat
+    
+    Validates user email address, allows them to ask questions and gives appropriate responses
+"""
+
+__author__ = "Christian Harborow"
+__email__  = "u1856364@unimail.hud.ac.uk"
+__date__   = "19/11/2018"
+
 
 def check_domain(email, domain):
     if email.count("@") != 1:
@@ -22,16 +32,17 @@ def find_word(question, word):
 operator_names = ["Dave", "John", "Sarah", "Abbey"]
 answers = ["Maybe", "Quite possibly", "I am unsure", "I will look into that"]
 uni_domain = "pop.ac.uk"
-user_email = input("Enter your email address: ")
+print("Welcome to the University of Poppleton's student support chat")
+user_email = input("Please enter your email address: ").lower()
+
 if not check_domain(user_email, uni_domain):
-    print("Email invalid, stopping client")
+    print("Not a valid university email address, stopping client")
 else:
-    print("Email valid")
+    print("Email address valid")
     print("You have been connected to", random.choice(operator_names))
     username = email_username(user_email)
-    print("Hi", username)
     while True:
-        response = input("How can I help you?: ").lower()
+        response = input("What do you need help with " + username + "?: ").lower()
         if find_word(response, "goodbye"):
             print("Stopping client")
             break
